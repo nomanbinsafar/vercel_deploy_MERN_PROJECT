dffdaf4d98bc4308eb33e6c5e04e16b874dd5762
nomanbinsafar <130387824+nomanbinsafar@users.noreply.github.com>
2024-07-06 02:38:06 +0600

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://vercel-deploy-mern-project-api.vercel.app/auth/login`;
+            const url = `https://vercel-deploy-mern-project-api-noman.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
